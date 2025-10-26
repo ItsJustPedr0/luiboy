706885311886a1dc9912d9417fbceb1d1f659204
ItsJustPedr0 <peterestacio09@gmail.com>
2025-10-26 20:01:27 +0800

    Project 3 progress report 2

--- Project 3/main.cpp
+++ Project 3/main.cpp
@@ -11,41 +11,109 @@
 
 using namespace std;
 
-void readMatrixFile()
+vector<vector<double>> readMatrixFile()
 {
   fstream matrixFile("matrix");
   string m;
-  int rowNumber;
-  int colNumber;
+  int rowNumber = 0;
+  int colNumber = 0;
 
-  while (getline(matrixFile, m))
+  vector<vector<double>> gaussMatrix;
+
+  while(!matrixFile.eof())
   {
+    getline(matrixFile, m);
     istringstream matrixRowStream(m);
     double matrixElement;
-    while(matrixRowStream >> matrixElement)
+
+    if (colNumber == 0)
     {
-      cout << matrixElement << " ";
+      vector<double> currentRow;
+      while(matrixRowStream >> matrixElement)
+      {
+        currentRow.push_back(matrixElement);
+        colNumber++;
+        cout << matrixElement << " | ";
+      }
+      gaussMatrix.push_back(currentRow);
+      cout << endl;
     }
-    cout << endl;
+    else if (rowNumber == colNumber - 1) break;
+    else 
+    {
+      vector<double> currentRow;
+      for(int i = 0; i < colNumber; i++)
+      {
+        matrixRowStream >> matrixElement;
+        currentRow.push_back(matrixElement);
+        cout << matrixElement << " | ";
+      }
+      gaussMatrix.push_back(currentRow);
+      cout << endl;
+    }
     rowNumber++;
   }
+  return gaussMatrix;
 }
 
-void gaussElimination()
+void gaussElimination(vector<vector<double>> A)
 {
-  int n;
-  for (int k = 1; k < (n-1); k++)
+  int n = A.capacity() - 1;
+  vector<double> x(A.capacity()); 
+
+  for (int k = 0; k < n; k++)
   {
     int m = k;
-    for (int j = k + 1; j < n; j++)
+    for (int j = k + 1; j < n + 1; j++)
     {
-      //insert code here
+      if (abs(A[m][k]) < abs(A[j][k])) m = j;
+    }
+    if (A[m][k] == 0)
+    {
+      cout << "No unique solution exists." << endl;
+      break;
+    }
+    else //exchange row k and m 
+    {
+      vector<double> temp = A[k];
+      A[k] = A[m];
+      A[m] = temp;
+    }
+    if (A[n][n] == 0)
+    {
+      cout << "No unique solution exists." << endl;
+      break;
+    }
+    else
+    {
+      for(int j = k + 1; j < n + 1; j++)
+      {
+        double test1 = A[j][k];
+        double test2 = A[k][k];
+        double m_jk = (A[j][k] / A[k][k]);
+        for (int p = k + 1; p < n + 2; p++)
+        {
+          A[j][p] = A[j][p] - (m_jk * A[k][p]);
+        }
+      }
     }
   }
+
+  x[n] = (A[n][n+1] / A[n][n]);
+
+  for (int i = (n - 1); i > -1; i--)
+  {
+    double sum = 0;
+    for(int j = i + 1; j < n + 1; j++) sum += A[i][j] * x[j];
+    x[i] = (1/A[i][i]) * (A[i][n+1] - sum);
+  }
+  for (int l = 0; l < x.capacity(); l++) cout << x[l] << endl;
 }
 
 int main()
 {
-  readMatrixFile();
-
+  cout << "Parsed Augmented Matrix:" << endl;
+  vector<vector<double>> gaussMatrix = readMatrixFile();
+  cout << endl << "Solution:" << endl;
+  gaussElimination(gaussMatrix);
 }
