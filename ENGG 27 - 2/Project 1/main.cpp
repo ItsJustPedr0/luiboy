5f66d70fe2e5a9b0a9e6e9212f589016ba112981
ItsJustPedr0 <peterestacio09@gmail.com>
2026-01-26 10:18:56 +0800

    Initial progress on Proj 1, Num Methods

--- ENGG 27 - 2/Project 1/main.cpp
+++ ENGG 27 - 2/Project 1/main.cpp
@@ -1,6 +1,12 @@
+// Peter Julius M. Estacio
+// ENGG 27.01 - M
+// Project 1 - Fixed-Point Approach for Finding Roots of Polynomials
+// main.cpp
+
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -19,10 +25,26 @@ int main()
     equation.insert(start, coeff);
     i++;
   }
-  while (i > -1)
+  cout << "=======================================" << endl;
+  cout << "Initial Guess (g_0): ";
+  double x, guess;
+  cin >> guess;
+  for (i; i > 0; i--)
   {
-    cout << equation[i];
-    if (i > 0) cout << "x^" << i << " + ";
-    i--;
+    cout << "=======================================" << endl;
+    cout << "Showing estimates for i = " << i << ":"<< endl;
+    x = guess;
+    for(int j = 0; j < 20; j++)
+    {
+      double g_x = 0;
+      for(int k = equation.size() - 1; k > -1; k--)
+      {
+        if(k != i) g_x += equation[k]*pow(x, k); 
+      }
+      g_x /= -equation[i]*pow(x, i-1); 
+      x = g_x;
+      cout << "g(" << j <<") = " << x << endl; 
+    }
   }
+  cout << "=======================================" << endl;
 }
