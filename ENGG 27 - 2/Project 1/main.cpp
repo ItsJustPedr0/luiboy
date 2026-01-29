252ee3f2f08c70e1205b302035c9bd6db417e831
ItsJustPedr0 <peterestacio09.com>
2026-01-29 16:45:17 +0800

    ENGG 27 Proj 1, comments and polynomial file formatting

--- ENGG 27 - 2/Project 1/main.cpp
+++ ENGG 27 - 2/Project 1/main.cpp
@@ -4,56 +4,98 @@
 // main.cpp
 
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <vector>
+#include <limits>
 #include <cmath>
 
 using namespace std;
 
 int main()
 {  
-  string fileName = "test";
-  // cin >> fileName;
-
-  fstream polynomialFile(fileName);
-  vector<double> equation;
-  double coeff;
-  int i = -1;
-  while (polynomialFile >> coeff)
+  string fileName; // takes user inut to select a polynomial file
+  cout << "Enter filename: "; // if no input, defaults to 'test'
+  if(!(cin >> fileName)) fileName = "test"; 
+  
+  fstream polynomialFile(fileName); 
+  if(!polynomialFile.is_open()) // returns error if file fails to open
   {
-    vector<double>::iterator start = equation.begin();  
-    equation.insert(start, coeff);
-    i++;
+    cout << "No file." << endl;
+    return 0;
   }
+
+  string s; // checks for the POLYNOMIAL token accdg to file format
+  if(!(polynomialFile >> s) && s != "POLYNOMIAL")
+  {
+    cout << "Invalid file, missing header." << endl;
+  }
+  else cout << "Header is present." << endl;
+
+  int equationIndex; // checks for eqn index accdg to file format
+  if(!(polynomialFile >> equationIndex))
+  {
+    cout << "Invalid file, missing polynomial index" << endl;
+  }
+  else cout << "Read polynomial of index " << equationIndex << endl;
+
+  // initializes array of coefficients
+  double equation[equationIndex+1]; 
+  double coeff;
+
+  // stores coefficients in equation array
+  for (int i = 0; i < equationIndex + 1; i++)  
+  {
+    polynomialFile >> coeff;
+    equation[i] = coeff;
+  }
+
+  // takes user input for initial guess g_0
   cout << "=======================================" << endl;
   cout << "Initial Guess (g_0): ";
   double x, guess;
   cin >> guess;
-  for (i; i > 0; i--)
+
+  // loop for all non-constant coeffs
+  for (int i = equationIndex; i > 0; i--) 
   {
     cout << "=======================================" << endl;
     cout << "Showing estimates for i = " << i << ":"<< endl;
     x = guess;
-    for(int j = 1; j < 50; j++)
+
+    // limited number of iterations to 100 for testing
+    for(int j = 0; j < 100; j++)
     {
+      // iterative algorithm for estimating the roots
       double g_x = 0;
-      for(int k = equation.size() - 1; k > -1; k--)
+      for(int k = equationIndex; k > -1; k--)
       {
         if(k != i) g_x += equation[k]*pow(x, k); 
       }
       g_x /= -equation[i]*pow(x, i-1); 
-      cout << "x_" << j <<" = " << g_x << endl; 
-      
+
+      // prints results per iteration on separate line
+      cout << "x_" << setprecision(6) << j <<" = " << g_x << endl; 
+
+      // sets maximum precision to display converging results
+      const int max_precision = numeric_limits<double>::max_digits10;
+
+      // checks for convergence via difference of last two results
       if (g_x - x == 0)
       {
-        cout << "Converged." << endl;
+        cout << "Converged. Final result: " 
+          << setprecision(max_precision) << x << endl;
         break;
       } 
+
+      //checks for divergence via nan or inf results
       else if (isnan(g_x) == true || isinf(g_x) == true) 
       {
-        cout << "Diverged." << endl;
+        cout << "Diverged. Iterations stopped." << endl;
         break;;
       }
+
+      // if neither, iterate
       else x = g_x;
     }
   }
