eca0f8ecc267d17350ef7cb678d5f721c092be3f
ItsJustPedr0 <peterestacio09@gmail.com>
2026-02-01 23:18:32 +0800

    ENGG 27 and 151 | validIntOrFloat HW

---
+++ ENGG 27 - 2/Homework/main.cpp
@@ -0,0 +1,25 @@
+#include "validIntOrFloat.h"
+
+int main()
+{
+  string testS[5] = {"abc", "123", "4.", "5.6a", "7.8e-2"};
+  int testI;
+  double testD;
+
+  for(int i = 0; i < 5; i++)
+  {
+    cout << "Test string: " << testS[i] << endl;
+
+    bool Iresult = is_int(testS[i], &testI);
+    cout << "Is int?: " << boolalpha << Iresult;
+    if (&testI != 0) cout << " | Value stored: " << testI << endl;
+    else cout << endl;
+
+
+    bool Dresult = is_floating_pt(testS[i], &testD);
+    cout << "Is double?: " << boolalpha << Dresult;
+    if (&testD != 0) cout << " | Value stored: " << testD << endl;
+    else cout << endl;
+    cout << endl;
+  }
+}
---
+++ ENGG 27 - 2/Homework/validIntOrFloat.h
@@ -0,0 +1,13 @@
+#ifndef validIntOrFloat
+#define validIntOrFloat
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+bool is_int(string s, int* value = 0);
+bool is_floating_pt(string s, double* value = 0);
+
+#endif
